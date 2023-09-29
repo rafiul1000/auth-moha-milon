3e39a3eef92767ecba19663a7bc3075a915a0649
rafiul1000 <rafiulislam5075@gmail.com>
2023-09-29 21:16:03 +0600

    Create User Context for shared authentication

--- src/__context_api.js
+++ src/__context_api.js
@@ -1,4 +1,6 @@
 /* 
-1. create context
-2. 
+1. create context (export)
+2. provider
+3. set value
+4. useContext
 */
---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,29 @@
+import { createContext } from "react";
+import PropTypes from 'prop-types';
+
+export const AuthContext = createContext(null);
+
+const AuthProvider = ({children}) => {
+    const authInfo = { name: 'nodi shagor, khal bil' }
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
+
+AuthProvider.propTypes = {
+    children: PropTypes.node
+}
+
+
+
+/* 
+1. create context & export it
+2. set provider with value
+3. use the Auth Provider in the main.jsx file
+4. access children in the AuthProvider component as children and use it in the middle of the provider
+
+*/
